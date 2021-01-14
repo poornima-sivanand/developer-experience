de2371530b4ca54babc1396a6d13accc4fddd264
Sivanand <poornima.sivanand@gov.bc.ca>
2021-01-14 13:45:41 -0800

    fix

--- apps/documize/pgsql/patroni/.pipeline/lib/config.js
+++ apps/documize/pgsql/patroni/.pipeline/lib/config.js
@@ -5,9 +5,10 @@ const version = '10'
 const name = 'patroni'
 
 const phases = {
-  build: {namespace:'bcgov-tools' , name: `${name}`, phase: 'build', changeId:changeId, suffix: `-build-${changeId}`, instance: `${name}-build-${changeId}`, tag:`v${version}-${changeId}`},
-   test: {namespace:`bcgov`,        name: `${name}`, phase: 'test' , changeId:changeId, suffix: '-test'             , instance: `${name}-test`             , tag:`v${version}-latest`},
-   prod: {namespace:`bcgov`,        name: `${name}`, phase: 'prod' , changeId:changeId, suffix: ''                  , instance: `${name}-prod`             , tag:`v${version}-stable`}
+  build: {namespace:'psbox10-tools' , name: `${name}`, phase: 'build', changeId:changeId, suffix: `-build-${changeId}`, instance: `${name}-build-${changeId}`, tag:`v${version}-${changeId}`},
+  dev: {namespace:'psbox10-dev' , name: `${name}`, phase: 'dev', changeId:changeId, suffix: `-dev-${changeId}`, instance: `${name}-dev-${changeId}`, tag:`v${version}-${changeId}`},
+   test: {namespace:`psbox10-dev`,        name: `${name}`, phase: 'test' , changeId:changeId, suffix: '-test'             , instance: `${name}-test`             , tag:`v${version}-latest`},
+   prod: {namespace:`psbox10-dev`,        name: `${name}`, phase: 'prod' , changeId:changeId, suffix: ''                  , instance: `${name}-prod`             , tag:`v${version}-stable`}
 }
 
 module.exports = exports = phases
