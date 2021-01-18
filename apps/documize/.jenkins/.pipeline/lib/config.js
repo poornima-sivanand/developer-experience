cff10e2d4ad8f0cced1928fc5d05e3b7e8319b16
Sivanand <poornima.sivanand@gov.bc.ca>
2021-01-18 13:22:39 -0800

    change namespace

--- apps/documize/.jenkins/.pipeline/lib/config.js
+++ apps/documize/.jenkins/.pipeline/lib/config.js
@@ -5,9 +5,9 @@ const version = '1.0.0'
 const name = 'jenkins'
 
 const phases = {
-  build: {namespace:'psbox10-tools'    , name: `${name}`, phase: 'build'  , changeId:changeId, suffix: `-build-${changeId}`  , instance: `${name}-build-${changeId}`  , version:`${version}-${changeId}`, tag:`build-${version}-${changeId}`},
-  dev: {namespace:'psbox10-tools'    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`},
-  prod: {namespace:'psbox10-tools'    , name: `${name}`, phase: 'prod'  , changeId:changeId, suffix: `-prod`  , instance: `${name}-prod`  , version:`${version}`, tag:`prod-${version}`},
+  build: {namespace:'101ed4-tools'    , name: `${name}`, phase: 'build'  , changeId:changeId, suffix: `-build-${changeId}`  , instance: `${name}-build-${changeId}`  , version:`${version}-${changeId}`, tag:`build-${version}-${changeId}`},
+  dev: {namespace:'101ed4-tools'    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`},
+  prod: {namespace:'101ed4-tools'    , name: `${name}`, phase: 'prod'  , changeId:changeId, suffix: `-prod`  , instance: `${name}-prod`  , version:`${version}`, tag:`prod-${version}`},
 };
 
 // This callback forces the node process to exit as failure.
